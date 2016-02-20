cc685070299736f29b1026c42dbcb85a556d8306
ggg <galingrudov@gmail.com>
2016-02-20 15:42:11 -0500

    Get interface information

--- server/src/UdpSniffer.c
+++ server/src/UdpSniffer.c
@@ -1,17 +1,83 @@
 #include <stdio.h>
-#include <pcap.h>
-
-
-int main(int argc, char *argv[])
+#include <stdlib.h>
+#include <pcap.h>  /* GIMME a libpcap plz! */
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/if_ether.h> /* includes net/ethernet.h */
+int interfaceInformation(char *dev, char *net, char *mask)
 {
-  char *dev, errbuf[PCAP_ERRBUF_SIZE];
-
+  char errbuf[PCAP_ERRBUF_SIZE];
+  bpf_u_int32 netp; /* ip          */
+  bpf_u_int32 maskp;/* subnet mask */
+  struct in_addr addr;
+int ret;   /* return code */
+  /* ask pcap to find a valid device for use to sniff on */
   dev = pcap_lookupdev(errbuf);
-  if (dev == NULL) 
+
+  /* error checking */
+  if(dev == NULL)
   {
-  fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-  return(2);
+   printf("%s\n",errbuf);
+   exit(1);
   }
-  printf("Device: %s\n", dev);
-  return(0);
+
+  /* print out device name */
+  printf("DEV: %s\n",dev);
+
+  /* ask pcap for the network address and mask of the device */
+  ret = pcap_lookupnet(dev,&netp,&maskp,errbuf);
+
+  if(ret == -1)
+  {
+   printf("%s\n",errbuf);
+   exit(1);
+  }
+
+  /* get the network address in a human readable form */
+  addr.s_addr = netp;
+  net = inet_ntoa(addr);
+
+  if(net == NULL)/* thanks Scott :-P */
+  {
+    perror("inet_ntoa");
+    exit(1);
+  }
+
+  printf("NET: %s\n",net);
+
+  /* do the same as above for the device's mask */
+  addr.s_addr = maskp;
+  mask = inet_ntoa(addr);
+  
+  if(mask == NULL)
+  {
+    perror("inet_ntoa");
+    exit(1);
+  }
+  
+  printf("MASK: %s\n",mask);
+
+}
+
+int main(int argc, char **argv)
+{
+  char *dev; /* name of the device to use */
+  char *net; /* dot notation of the network address */
+  char *mask;/* dot notation of the network mask    */
+  int ret;   /* return code */
+  pcap_t* descr; //pcap descriptor
+  struct pcap_pkthdr hdr;     /* pcap.h */
+  struct ether_header *eptr;  /* net/ethernet.h */
+  char errbuf[PCAP_ERRBUF_SIZE];
+  const u_char *packet;
+  ret = interfaceInformation(dev, net, mask);
+  if (ret == -1)
+  {
+    perror("cannot get interface information. Are you root?");
+    exit(1);
+  }
+
+  return 0;
 }
