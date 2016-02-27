1fd8617410b5cecea4c8f62a71746d70e4b83a7c
root <root@debian>
2016-02-27 10:12:46 -0500

    Start server pert in client.pl in separete process. Check user input.

--- client/client.pl
+++ client/client.pl
@@ -5,6 +5,10 @@ use Net::RawIP;
 use Sys::Info;
 use Sys::Info::Constants qw( :device_cpu );
 use IO::Socket;
+
+# Flushing to STDOUT after each write
+$| = 1;
+
 # menu function 
   sub help 
   {
@@ -14,7 +18,7 @@ use IO::Socket;
           " local port: if not specified local port will be the same as destination port\n";
   }
 
-# take number of arguments
+  # take number of arguments
   my $num_arg = $#ARGV +1;
 #collect arguments
   (my $dest, my $dst_port, my $src_port)= @ARGV;
@@ -32,13 +36,48 @@ use IO::Socket;
     $src_port = $dst_port;
   }
 
-
-
+#validate input
+  my $is_valid = 1;
+  if(!($dest=~/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/ &&(($1<=255  && $2<=255 && $3<=255  &&$4<=255 ))))
+  {
+     $is_valid = 0;
+  }
+  if(!($dst_port=~/^([0-9]{1,5})$/ && ($1 <=65535)))
+ {
+   $is_valid = 0;
+  }
+  if(!($src_port=~/^([0-9]{1,5})$/ && ($1 <=65535)))
+  {
+     $is_valid = 0;
+  }
+  if ($is_valid == 0)
+  {
+    print "invalid Ip or port";
+    help();
+    exit;
+  }
 
    my $info = Sys::Info->new;
    my $cpu  = $info->device('CPU');
    printf "cpu count = %d\n", $cpu->count;
    my $cpu_str = sprintf("%d",$cpu->count);
+
+
+   my $pid = fork;
+   # if it is a chlid process 
+   if (not $pid )
+   {
+     # get eth0 information using unix command 'ifconfig'
+     my $IP_eth0 = `ifconfig eth0`;
+     #catch the adress. the result is in the first catch ->$1
+     $IP_eth0 =~ /.*inet addr:(.*)  Bcast:/;
+     $IP_eth0 = $1;
+     # Server side information
+     server($IP_eth0, $src_port, $cpu);
+     exit;     
+   }
+
+   sleep(2);# giving 2 sec to the server to start 
    my $n = Net::RawIP->new({
                         ip  => {
 #                                saddr => ',
@@ -52,22 +91,17 @@ use IO::Socket;
                                 data  => $cpu_str,
                                },
                        });
+  print "Sending information\n";
   $n->send(1,1);
-
-# get eth0 information using unix command 'ifconfig'
-  my $IP_eth0 = `ifconfig eth0`;
-#catch the adress the result is in the first catch ->$1
-  $IP_eth0 =~ /.*inet addr:(.*)  Bcast:/;
-  $IP_eth0 = $1;
+# waiting for the child process to finish
+  my $g = wait();
 
 
-# Server side information
-  server($IP_eth0, $src_port);
   sub server
   {
 # copy values of @_ in local variables
 # Within a subroutine the array @_ contains the parameters passed to that subroutine. 
-    my ($host,$listen_port)= @_;
+    my ($host,$listen_port, $cpu)= @_;
 
     my $protocal        = 'udp';
     my $received_data   = undef;
@@ -90,7 +124,7 @@ use IO::Socket;
      my $peer_address = $server->peerhost();
      my $peer_port    = $server->peerport();
      my $sock_port    = $server->sockport();
-     print "Message was received from: $peer_address,to port:$sock_port, data: $received_data\n";
+     print "Message was received from: $peer_address,to port:$sock_port, pid_id: $received_data\n";
    }
 
    print "Closing socket...\n";
